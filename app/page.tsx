b61ab6fa741c0eaba4e89586513d3f632ba2d4d9
Dave Thomson <djthomson@gmail.com>
2024-06-13 10:00:51 +0100

    Implement shopping list data structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+type ShoppingItem = {
+  id: number;
+  title: string;
+  quantity: number;
+  price: number;
+  order: number;
+}
+
+type ShoppingList = ShoppingItem[]
 
 export default function Home() {
+
+  const shoppingList: ShoppingList = [
+    { id: 1, title: 'Apple', quantity: 2, price: 0.88, order: 1},
+    { id: 2, title: 'Oranges', quantity: 3, price: 0.90, order: 2},
+  ]
 
   return (
     <div className="w-1/2 p-4 mt-8 mx-auto border">
