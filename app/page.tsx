64d736cede3c18bc38e3ed3bb501d4caed337aac
Dave Thomson <djthomson@gmail.com>
2024-06-13 18:40:33 +0100

    Add shopping list reducer to add item

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { ShoppingListType } from '../types'
 export default function Home() {
 
   const shoppingList: ShoppingListType = [
-    { id: 1, title: 'Apple', quantity: 2, price: 0.88, order: 1},
-    { id: 2, title: 'Oranges', quantity: 3, price: 0.90, order: 2},
+    { id: 1, title: 'Apples', quantity: 2, price: 0.60},
+    { id: 2, title: 'Oranges', quantity: 3, price: 0.90},
   ]
 
   return (
--- features/shopping-list/components/ShoppingList.tsx
+++ features/shopping-list/components/ShoppingList.tsx
@@ -1,43 +1,58 @@
-import { useState } from 'react'
-import { ShoppingListType, ShoppingItemType } from '../../../types'
+'use client'
+import { useReducer } from 'react'
+import { ShoppingListType, NewShoppingItemType } from '../../../types'
 import ItemForm from './ItemForm'
+import CurrencyFormat from '../../ui/CurrencyFormat'
 
 type ShoppingListProps = {
   title: string;
   shoppingList: ShoppingListType;
 }
 
-const LOCALE = 'en-GB'
-const CURRENCY_CODE = 'GBP'
+export default function ShoppingList({ title, shoppingList }: ShoppingListProps) {
 
-export default async function ShoppingList({ title, shoppingList }: ShoppingListProps) {
+  const [state, dispatch] = useReducer(shoppingListReducer, shoppingList)
 
-  const currency = Intl.NumberFormat(LOCALE, {
-    style: 'currency',
-    currency: CURRENCY_CODE,
-  });
-
-  async function addItem(item: ShoppingItemType) {
-    'use server'
-    console.log('ADDED ITEM', item)
-    return null
+  function handleAddItem(item: NewShoppingItemType) {
+    dispatch({
+      type: 'add_item',
+      item
+    });
   }
 
   return (
     <>
-      <ItemForm items={shoppingList} addItem={addItem} />
+      <ItemForm items={shoppingList} addItem={handleAddItem} />
 
+      {/* Shopping List */}
       <div className="sm:w-1/2 p-4 mt-8 mx-auto sm:border">
         <h1 role="heading" className="text-xl mb-4">{title}</h1>
         <ul>
-          {shoppingList.map(({ id, title, quantity, price }) => (
+          {state.map(({ id, title, quantity, price }) => (
             <li className="flex justify-between" key={id}>
               <span><input type="checkbox" className="mr-2" />{title} x{quantity}</span>
-              <span>{currency.format(price)}</span>
+              <CurrencyFormat value={price} />
             </li>
           ))}
         </ul>
       </div>
     </>
   )
+}
+
+export function shoppingListReducer(state: ShoppingListType, action: any) {
+  switch (action.type) {
+    case 'add_item': {
+      const nextId = state.length + 1
+      const newItem = {
+        id: nextId,
+        ...action.item,
+      }
+      return [
+        ...state,
+        newItem
+      ]
+    }
+  }
+  throw Error('Unknown action: ' + action.type);
 }
