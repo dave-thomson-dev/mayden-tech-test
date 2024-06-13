99c65059b1868ac814f085ddb2882c9d057c9223
Dave Thomson <djthomson@gmail.com>
2024-06-13 09:58:21 +0100

    Add basic ui for shopping list

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
 
 export default function Home() {
+
   return (
-    <h1 role="heading">Shopping List</h1>
+    <div className="w-1/2 p-4 mt-8 mx-auto border">
+      <h1 role="heading" className="text-xl mb-4">Shopping List</h1>
+      <ul>
+        <li className="flex justify-between">
+          <span><input type="checkbox" className="mr-2" /> Apples x2</span>
+          <span>£1</span>
+        </li>
+        <li className="flex justify-between">
+          <span><input type="checkbox" className="mr-2" /> The quick brown fox jumps over the lazy dog sadf asdf asdf asdf  x2</span>
+          <span>£1</span>
+        </li>
+      </ul>
+    </div>
   )
 }
